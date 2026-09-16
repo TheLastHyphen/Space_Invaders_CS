71882a032ce0a57cdbc1e3f809039312b352aede
David Nicholas <davidnic@live.co.uk>
2026-09-16 13:02:57 +0100

    Added event to notify Game object that it has finished drawing the invaders. Added event to notify Game object about how many invaders have been drawn.

--- Globals/SignalBroadCaster.cs
+++ Globals/SignalBroadCaster.cs
@@ -6,6 +6,8 @@ public partial class SignalBroadCaster : Node
 	public static SignalBroadCaster Instance { get; private set; }
 
 	public Action OnMoveTimerTimeOut;
+	public Action OnFinishedDrawingInvaders;
+	public Action<int> OnUpdateNumberOfInvaders;
 
 	public override void _Ready()
 	{
@@ -15,5 +17,15 @@ public partial class SignalBroadCaster : Node
 	public void EmitOnMoveTimerTimeOut()
 	{
 		OnMoveTimerTimeOut?.Invoke();
+	}
+
+	public void EmitOnFinishedDrawingInvaders()
+	{
+		OnFinishedDrawingInvaders?.Invoke();
+	}
+
+	public void EmitOnUpdateNumberOfInvaders(int number)
+	{
+		OnUpdateNumberOfInvaders?.Invoke(number);
 	}
 }
