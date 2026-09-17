58c1da887fd77743b8367213b4ff06fa34069c3b
David Nicholas <davidnic@live.co.uk>
2026-09-17 14:36:54 +0100

    File name. Scene changes.

---
+++ Globals/PackedScenes.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class PackedScenes : Node
+{
+	public static PackedScenes Instance { get; private set; }
+
+	public PackedScene level = ResourceLoader.Load<PackedScene>("res://Scenes/Level/Level.tscn");
+	public PackedScene InvaderSquid = ResourceLoader.Load<PackedScene>("res://Scenes/InvaderSquid/InvaderSquid.tscn");
+	public PackedScene InvaderCrab = ResourceLoader.Load<PackedScene>("res://Scenes/InvaderCrab/InvaderCrab.tscn");
+	public PackedScene InvaderOctopus = ResourceLoader.Load<PackedScene>("res://Scenes/InvaderOctopus/InvaderOctopus.tscn");
+	public PackedScene Player = ResourceLoader.Load<PackedScene>("res://Scenes/Player/Player.tscn");
+	public PackedScene Game = ResourceLoader.Load<PackedScene>("res://Scenes/Game/Game.tscn");
+	public PackedScene MainUI = ResourceLoader.Load<PackedScene>("res://Scenes/MainUI/MainUi.tscn");
+	
+	public override void _Ready()
+	{
+		Instance = this;
+	}
+}
