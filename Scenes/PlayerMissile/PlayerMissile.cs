9c47083bb91d17021891f396f7cd9f63da6b14a8
David Nicholas <davidnic@live.co.uk>
2026-09-19 15:51:06 +0100

    Some small changes.

--- Scenes/PlayerMissile/PlayerMissile.cs
+++ Scenes/PlayerMissile/PlayerMissile.cs
@@ -9,38 +9,26 @@ public partial class PlayerMissile : Area2D
 	public override void _Ready()
 	{
 		base._Ready();
-		SignalBroadCaster.Instance.OnPlayerFirePressed += OnPlayerFirePressed;
 	}
 
 	public override void _ExitTree()
 	{
 		base._ExitTree();
-		SignalBroadCaster.Instance.OnPlayerFirePressed -= OnPlayerFirePressed;
 	}
 
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-		if(_isFired == true)
-		{
-			Vector2 pos = Position;
-			pos.Y -= 1 * (float)delta * _missileSpeed;
-			Position = pos;
-		}
-	}
-
-	public void OnPlayerFirePressed(Vector2 pos)
-	{
-		if(_isFired == true) return;
+		Vector2 pos = Position;
+		pos.Y -= 1 * (float)delta * _missileSpeed;
 		Position = pos;
-		_isFired = true;
 	}
 
 	public void OnAreaEntered(Area2D area)
 	{
 		if(area.Name == "CeilingBoundary")
 		{
-			_isFired = false;
+			QueueFree();
 		}
 	}
 }
