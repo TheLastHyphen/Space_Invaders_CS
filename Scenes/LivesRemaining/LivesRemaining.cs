42613d428a67ff145d65d1315a039bb3dfd90664
David Nicholas <davidnic@live.co.uk>
2026-09-20 13:13:44 +0100

    Added LivesRemaining scene. This visually handles the live remaining.

---
+++ Scenes/LivesRemaining/LivesRemaining.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public partial class LivesRemaining : Node2D
+{
+	private TextureRect _life1;
+	private TextureRect _life2;
+	private int _remainingLives = 3;
+	private Label _liveLabel;
+
+	public override void _Ready()
+	{
+		_life1 = GetNode<TextureRect>("HBoxContainer/Life1");
+		_life2 = GetNode<TextureRect>("HBoxContainer/Life2");
+		_liveLabel = GetNode<Label>("HBoxContainer/LivesLabel");
+		_liveLabel.Text = "3";
+		SignalBroadCaster.Instance.OnPlayerHit += OnAreaEntered;
+	}
+
+	public void UpdateLives()
+	{
+		_remainingLives -= 1;
+		switch(_remainingLives)
+		{
+			case 0: _liveLabel.Text = "0";
+					break;
+			case 1: _life2.Hide();
+					_liveLabel.Text = "1";
+					break;
+			case 2: _life1.Hide();
+					_liveLabel.Text = "2";
+					break;
+		}
+	}
+
+	private void OnAreaEntered()
+	{
+		UpdateLives();
+	}
+}
