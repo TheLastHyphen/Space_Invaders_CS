7f2c3fda2d2a5e7ed533cee89c20fd3bc073caff
David Nicholas <davidnic@live.co.uk>
2026-09-20 11:39:55 +0100

    Set _canFire to false. This is set to true once a signal is received. It was found that the player could fire and hit the invaders while they were still being drawn.

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -5,7 +5,7 @@ public partial class Player : Area2D
 	private Vector2 _playerPosition = new Vector2(238,558);
 	private float playerSpeed = 500.0f;
 	private bool _canMove = true;
-	private bool _canFire = true;
+	private bool _canFire = false;
 	private float _playerBaseWidth = 0.0f;
 	private Area2D missile = null;
 	
@@ -20,11 +20,13 @@ public partial class Player : Area2D
 		base._Ready();
 		Position = _playerPosition;
 		_playerBaseWidth = GetNode<Sprite2D>("PlayerSprite").Texture.GetSize().X;
+		SignalBroadCaster.Instance.OnCanFireMissile += OnCanFireMissile;
 	}
 
 	public override void _UnhandledInput(InputEvent inputEvent)
 	{
 		base._UnhandledInput(inputEvent);
+		if(_canFire == false) return;
 		if(inputEvent.IsActionPressed("PlayerFire") && GodotObject.IsInstanceValid(missile) == false)
 		{
 			missile = PackedScenes.Instance.PlayerMissile.Instantiate<PlayerMissile>();
@@ -49,5 +51,10 @@ public partial class Player : Area2D
 			pos.X = Game.LeftBoundaryX;
 		}
 		Position = pos;
+	}
+
+	public void OnCanFireMissile()
+	{
+		_canFire = true;
 	}
 }
