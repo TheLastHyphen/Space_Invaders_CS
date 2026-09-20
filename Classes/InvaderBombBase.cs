c4d5009fae6ddf23642c256847817aff8b16d4ff
David Nicholas <davidnic@live.co.uk>
2026-09-20 11:35:57 +0100

    Added abstract property for Points.

--- Classes/InvaderBombBase.cs
+++ Classes/InvaderBombBase.cs
@@ -6,6 +6,8 @@ public abstract partial class InvaderBombBase : Area2D
 	private int _speed = 0;
 	public AnimatedSprite2D InvAnimatedSprite => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	
+	public abstract int Points { get; }
+
 	public override void _Ready()
 	{
 		_speed = (int)GD.RandRange(75.0f, 400.0f);
